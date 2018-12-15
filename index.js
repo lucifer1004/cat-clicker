e8fe6c07571df8c4361dc69831f49ac69458944b
Gabriel Wu <qqbbnease1004@126.com>
2018-12-16 07:59:03 +0800

    version 2.0: multiple cats

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-let count = 0
+const catSources = []
+catSources.push({'src': 'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'})
+catSources.push({'src': 'https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'})
 
-const countText = () => {
+const countText = (count) => {
   return '<p>' +
     `This cat has been clicked ${count} ${
              count === 1 ? 'time' : 'times'}.` + '</p>';
 };
 
-const render = () => {
+const count = []
+const text = []
+let index = 0
+catSources.forEach(cat => {
+  count.push(0)
+  const div = document.createElement('div')
+  const img = document.createElement('img')
+  img.src = cat.src
+  img.id = index
+  div.appendChild(img)
+  document.body.appendChild(div)
+
+  const textDiv = document.createElement('div')
+  textDiv.id = index
+  textDiv.render = function() {
+    this.innerHTML = countText(count[this.id])
+  }
+  document.body.appendChild(textDiv)
   textDiv.render()
-}
+  text.push(textDiv)
 
-const div = document.createElement('div')
-const img = document.createElement('img')
-img.src =
-  'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-img.style.width = '100%'
-div.appendChild(img)
-document.body.appendChild(div)
+  img.addEventListener('click', function() {
+    count[this.id]++
+    text[this.id].render()
+  })
 
-img.addEventListener('click', () => {
-  count++
-  render()
+  index++
 })
-
-const textDiv = document.createElement('div')
-textDiv.render = () => {
-  textDiv.innerHTML = countText()
-}
-render()
-document.body.appendChild(textDiv)
